3cdd2876973324e9173ffc2ec10093dc18e0142d
sandeep <sandeep@ashmar.in>
2019-01-11 13:34:21 +0530

    some changes

--- app/src/main/java/com/example/monet_android1/contactdetailsdemo/fragment/ContactsFragment.java
+++ app/src/main/java/com/example/monet_android1/contactdetailsdemo/fragment/ContactsFragment.java
@@ -1,7 +1,6 @@
 package com.example.monet_android1.contactdetailsdemo.fragment;
 
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
